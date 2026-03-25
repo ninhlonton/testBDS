731e974433c7ee75c7ff3911647216b36c176ce0
ninhlonton <mahuuninh2006@gmail.com>
2026-03-25 21:33:39 +0700

    first commit

---
+++ django_real-estate-main/README.md
@@ -0,0 +1,46 @@
+## About
+### Real Estate Multi-page Application with Admin area
+- Search the property using keywords and search filters <br>
+- Get detailed information about chosen property <br>
+- Pagination implemented <br>
+- Login/Logout <br>
+
+https://user-images.githubusercontent.com/94207798/196047006-c6be3f42-4b6f-41a4-bc6f-3a98f6294a9d.mp4
+
+## Setup
+
+1. The first thing to do is to clone the repository:
+
+```sh
+$ git clone https://github.com/AnastasiaLunina/django_real-estate.git
+$ cd realestate
+```
+
+2. Create a virtual environment to install dependencies in and activate it:
+
+```sh
+$ python3 -m venv ./env
+
+# Mac/Linux
+$ source env/bin/activate
+
+# Windows
+venv\Scripts\activate.bat
+
+# Exit venv
+deactivate
+```
+3. In settings.py change password for PostrgreSQL. 
+
+4. Then install the dependencies:
+
+```sh
+(env)$ pip install -r requirements.txt
+```
+
+5. Once `pip` has finished downloading the dependencies:
+```sh
+(env)$ cd project
+(env)$ python manage.py runserver
+```
+6. And navigate to `http://127.0.0.1:8000/
---
+++ django_real-estate-main/contacts/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.core.mail import send_mail
+from .models import Contact
+
+
+def contact(request):
+    if request.method == 'POST':
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+        realtor_email = request.POST['realtor_email']
+
+        # Check if user made an inquiry already
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(request, 'Looks like you are already asked about this one')
+                return redirect('/listings/'+listing_id)
+
+        contact = Contact(listing=listing,
+                          listing_id=listing_id,
+                          name=name,
+                          email=email,
+                          phone=phone,
+                          message=message,
+                          user_id=user_id)
+        contact.save()
+
+        send_mail(
+            'Realestate Listing Inquiry ' + listing,
+            'Someone is interested in ' + listing + '. ' 'Sign in to admin panel to see the details',
+            'lunina.anastasiya@gmail.com',
+            [realtor_email, 'lunina-anastasiya@yandex.ru'],
+            fail_silently=False
+        )
+
+        messages.success(request, 'We got it and starting to work on it. Will get back to you shortly')
+        return redirect('/listings/'+listing_id)
