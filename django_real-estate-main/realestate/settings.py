b691c4f82a7774e0d15b67f6007b41e8fbb13729
ninhlonton <mahuuninh2006@gmail.com>
2026-03-26 07:42:12 +0700

    first upload

--- django_real-estate-main/realestate/settings.py
+++ django_real-estate-main/realestate/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-xnsy6gzvfc-b*@g_rf==5@h933!ri%&w)j+ck8qimch-_sipot
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.5', '127.0.0.1', 'localhost']
 
 
 # Application definition
